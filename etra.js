21917c0a367b10c2e7ccf89175e670eebf9c6d4c
Hickacou <hickacou@gmail.com>
2021-02-08 21:06:46 +0100

    initial: first and (probably) last commit

---
+++ etra.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const fs = require('fs');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('Connected as ' + client.user.tag);
+});
+
+client.on('message', async message => {
+	if (message.author.id !== client.user.id)
+		return;
+	if (message.content === 'etra help')
+		return message.channel.send('https://hicka.dev/etra#readme');
+	if (message.content === 'etra save') {
+		const emojis = message.guild.emojis.map(e => new Object({ 'animated': e.animated, 'name': e.name, 'url': e.url }));
+		fs.writeFileSync(`${message.guild.id}.json`, JSON.stringify(emojis));
+		const g = countNecessaryGuilds(emojis);
+		message.channel.send(`\`Data saved (${emojis.length} elements).\`\n> **Run etra copy <id> to copy the elements automatically** *(Will require ${g} guild${g > 1 ? 's' : ''} for ${message.guild.name})*`);
+	}
+	if (message.content === 'etra copy') {
+		// Will commit this shit later
+	}
+
+});
+
+
+function countNecessaryGuilds(emojis) {
+	let animated = emojis.filter(e => e.animated).length;
+	let static = emojis.length - animated;
+	let guilds = 0;
+	while (animated > 0 || static > 0) {
+		animated -= 50;
+		static -= 50;
+		guilds++;
+	}
+	return guilds;
+}
+
+client.login(require('./token.json').token);
---
+++ readme.md
@@ -0,0 +1,12 @@
+# etra
+## Emoji Transfer
+Small script to save and copy emojis from a Discord Server. 
+
+### How To Use
+- Create `token.json` and put `{"token":"YOUR_TOKEN"}` inside of it.
+- Run `npm install discord.js@11`
+- Run the script (`node etra.js` if you don't know)
+- Read the code to know what to do next
+
+### CAUTION
+> This uses Discord.js v11 to allow self-bots. However, self-bots are forbidden by Discord, so doing this can end up getting your account banned. I'm not responsible of any loss. Play at your own risks.
